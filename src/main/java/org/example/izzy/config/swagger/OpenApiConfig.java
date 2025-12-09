cb537400feed683f55c6081d153c0d938490d28b
athimjon <tonym5one@gmail.com>
2025-12-10 01:07:53 +0500

    Swagger configured

--- src/main/java/org/example/izzy/config/swagger/OpenApiConfig.java
+++ src/main/java/org/example/izzy/config/swagger/OpenApiConfig.java
@@ -12,8 +12,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "IZZY E-COMMERCE  ", version = "v1", description = "PROJECT"),
-        servers = @Server(url = "http://localhost:8080"),
+        info = @Info(title = "Vault 📂\uD83D\uDDC2 ", version = "v1", description = "PROJECT"),
+        servers = @Server(url = "http://13.62.54.193:8080"),
         security = @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = "bearerAuth")
 
 )
